fbd04cd2103e2229b5fe25edcee16e0c2900680d
Lautaro <forzanolautaro@gmail.com>
2023-12-03 13:45:03 -0300

    probando

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 /*** Algoritmo con un condicional ***/
 document.getElementById("c1").addEventListener("click", function () {
-  alert("Atención. Este sitio es para mayores de 18 años.");
   const nombre = prompt("Ingrese su nombre.");
-  const edad = parseInt(prompt("Ingrese su edad en números."));
-  if (edad >= 18) {
-    console.log(nombre + " es mayor, puede ingresar al sitio.");
-    alert("Hola " + nombre + ", podés ingresar.");
+  alert("Bienvenido " + nombre + " al simulador de plazos fijos.");
+  console.log(nombre + " quiere ingresar al simulador de plazos fijos.");
+  const pin = parseInt(prompt("Ingrese su PIN para acceder. (6666)"));
+
+  if (pin === 6666) {
+    alert("Pin correcto. Accediendo al simulador de plazos fijos.");
+    console.log("Pin correcto.");
+    console.log(nombre + " ingresó al simulador de plazos fijos.");
+
+    const plazoFijo = prompt("¿Desea simular un Plazo Fijo? (si/no)");
+    if (plazoFijo.toLowerCase() === "si") {
+      alert("Próximamente podrás simular un Plazo Fijo.");
+      console.log("Accedió a simular un plazo fijo.");
+      console.log("El simulador de plazos fijos no está disponible.");
+      console.log("Sesión cerrada.");
+    } else {
+      alert("Se cerró tu sesión.");
+      console.log(nombre + " no accedió a simular un plazo fijo.");
+      console.log("Sesión cerrada.");
+    }
   } else {
-    console.log(nombre + " es menor, no tiene permitido el acceso al sitio.");
-    alert("Lo sentimos " + nombre + ", no tenes permitido el ingreso.");
+    alert("Pin incorrecto. Acceso denegado.");
+    console.log(nombre + " ha ingresado un pin incorrecto.");
+    console.log("Acceso denegado.");
   }
 });
 
